19df9c049c9af854e7d5dcc17abb976a4ac50962
Akhil554 <165761826+Akhil554@users.noreply.github.com>
2025-12-09 18:31:33 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -158,5 +158,7 @@ def get_students():
     ]
     return jsonify(result)
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+import os
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
